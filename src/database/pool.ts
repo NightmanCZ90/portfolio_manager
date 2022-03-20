2622e35b3613a156e6ced7eb756036c489f7f365
NightmanCZ90 <novak.jan90@gmail.com>
2022-03-20 19:28:48 +0100

    feat: Connect to pg database

---
+++ src/database/pool.ts
@@ -0,0 +1,24 @@
+import pg from 'pg';
+
+// wrapping Pool for testing purposes
+// and being able to reconnect to a different database
+class Pool {
+  _pool: pg.Pool | null = null;
+
+  connect(options: pg.PoolConfig) {
+    this._pool = new pg.Pool(options);
+    return this._pool.query('SELECT 1 + 1;');
+  }
+
+  close() {
+    if (!this._pool) return;
+    return this._pool.end();
+  }
+
+  query(sql: pg.QueryArrayConfig, params: any) {
+    if (!this._pool) return;
+    return this._pool.query(sql, params);
+  }
+}
+
+export default new Pool();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
 import app from './server';
+import pool from './database/pool';
+import config from './config';
 
-const port = Number(process.env.PORT || 8080);
-app.listen(port, () => {
-  console.log('Listening on port: ' + port);
-});
+const port = Number(config.port || 8080);
+
+pool.connect({
+  host: config.pgHost,
+  port: config.pgPort,
+  database: config.pgDatabase,
+  user: config.pgUser,
+  password: config.pgPassword,
+})
+  .then(() => {
+    app.listen(port, () => {
+      console.log('Listening on port: ' + port);
+    });
+  })
+  .catch((err) => console.error(err));
