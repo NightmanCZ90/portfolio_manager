7e83a410835991d7161e2dcb54b2e5adb83684e1
NightmanCZ90 <novak.jan90@gmail.com>
2022-03-21 23:01:07 +0100

    feat: Implement authentication for users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
 import usersController from '../controllers/users';
+import isAuth from '../middlewares/is-auth';
 
 const router = Router();
 
-router.get('/users', usersController.getAllUsers);
+router.get('/users', isAuth, usersController.getAllUsers);
 
 export default router;
