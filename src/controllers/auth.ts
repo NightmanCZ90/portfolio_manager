1b2faa3b98215db22bee3dfe648f1991fca90d3e
NightmanCZ90 <novak.jan90@gmail.com>
2022-03-21 20:08:11 +0100

    feat: Login user feature

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,19 +1,20 @@
 import { validationResult } from 'express-validator';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Response } from 'express';
 
 import { User } from '../models/user';
 import UserRepo from '../repos/user-repo';
 import { TypedRequestBody } from '../models/routes';
 import { ValidationError } from '../server';
+import { Auth } from '../models/auth';
 
 export default {
   signup: async (req: TypedRequestBody<User>, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     try {
       if (!errors.isEmpty()) {
-        const error: ValidationError = { ...new Error('Validation failed.')};
+        const error: ValidationError = new Error('Validation failed.');
         error.statusCode = 422;
         error.data = errors.array();
         throw error;
@@ -34,5 +35,40 @@ export default {
       }
       next(err);
     };
-  }
+  },
+  login: async (req: TypedRequestBody<Auth>, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await UserRepo.findByEmail(email);
+
+      if (!user) {
+        const error: ValidationError = new Error('A user with this email does not exist.');
+        error.statusCode = 401;
+        throw error;
+      }
+      const isEqual = await bcrypt.compare(password, user.password);
+      if (!isEqual) {
+        const error: ValidationError = new Error('Wrong password.');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: user.email,
+          userId: user.id,
+        },
+        'somesupersecretstring',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({ token, userId: user.id });
+      return;
+    } catch (err: any) {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+      return err;
+    };
+  },
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -39,4 +39,6 @@ router.post('/signup', [
     })
 ], authController.signup);
 
+router.post('/login', authController.login);
+
 export default router;
