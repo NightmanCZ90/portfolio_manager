456e67db7aacd148fd555d2dd6cda266b8a6ea76
NightmanCZ90 <novak.jan90@gmail.com>
2022-03-22 22:50:05 +0100

    refactor: Make user validation reusable

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,30 +1,55 @@
 import { NextFunction, Request, Response } from 'express';
-import { AuthRequest } from '../models/routes';
+import { validationResult } from 'express-validator';
+import { AuthRequest, AuthRequestBody } from '../models/routes';
+import { User } from '../models/user';
 import UserRepo from '../repos/user-repo';
 import { StatusError } from '../server';
 
+const isCurrentUser = async (req: AuthRequest) => {
+  const { id } = req.params;
+  const userId = parseInt(id);
+  const user = await UserRepo.findById(userId);
+
+  if (req.body.userId !== userId) {
+    const error: StatusError = new Error('Not authenticated.');
+    error.statusCode = 403;
+    throw error;
+  }
+
+  if (!user) {
+    const error: StatusError = new Error('User with this id does not exist.');
+    error.statusCode = 404;
+    throw error;
+  }
+
+  return user;
+}
+
 const usersController = {
   getAllUsers: async (req: Request, res: Response, next: NextFunction) => {
-    const users = await UserRepo.find();
-    res.status(200).json({ users });
+    try {
+      const users = await UserRepo.find();
+      res.status(200).json({ users });
+    } catch (err: any) {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    }
   },
+
   getUser: async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
-      const { id } = req.params;
-      const userId = parseInt(id);
-      const user = await UserRepo.findById(userId);
+      const errors = validationResult(req);
 
-      if (!user) {
-        const error: StatusError = new Error('User with this id does not exist.');
-        error.statusCode = 404;
+      if (!errors.isEmpty()) {
+        const error: StatusError = new Error('Validation failed.');
+        error.statusCode = 422;
+        error.data = errors.array();
         throw error;
       }
+      const user = await isCurrentUser(req);
 
-      if (req.body.userId !== userId) {
-        const error: StatusError = new Error('Not authenticated.');
-        error.statusCode = 403;
-        throw error;
-      }
       res.status(200).json({ user });
     } catch (err: any) {
       if (!err.statusCode) {
