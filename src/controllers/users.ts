34e5f23704ee2ba0fc94ee565ff0a32349c0cac3
NightmanCZ90 <novak.jan90@gmail.com>
2022-03-22 01:02:54 +0100

    feat: Add getUser controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,10 +1,37 @@
 import { NextFunction, Request, Response } from 'express';
+import { AuthRequest } from '../models/routes';
 import UserRepo from '../repos/user-repo';
+import { StatusError } from '../server';
 
 const usersController = {
   getAllUsers: async (req: Request, res: Response, next: NextFunction) => {
     const users = await UserRepo.find();
     res.status(200).json({ users });
+  },
+  getUser: async (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+      const userId = parseInt(id);
+      const user = await UserRepo.findById(userId);
+
+      if (!user) {
+        const error: StatusError = new Error('User with this id does not exist.');
+        error.statusCode = 404;
+        throw error;
+      }
+
+      if (req.body.userId !== userId) {
+        const error: StatusError = new Error('Not authenticated.');
+        error.statusCode = 403;
+        throw error;
+      }
+      res.status(200).json({ user });
+    } catch (err: any) {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    };
   },
 
   // TODO: get users you manage
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,6 +5,11 @@ import isAuth from '../middlewares/is-auth';
 
 const router = Router();
 
+// TODO: Refactor to only show users you manage with limited data
 router.get('/users', isAuth, usersController.getAllUsers);
+
+router.get('/users/:id', isAuth, usersController.getUser);
+
+router.post('/users/:id', isAuth, usersController.getAllUsers);
 
 export default router;
