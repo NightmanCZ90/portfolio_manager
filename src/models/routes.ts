7c1a2a5b40bff8330c664f481576e6fcb3143828
NightmanCZ90 <novak.jan90@gmail.com>
2022-03-22 01:01:54 +0100

    refactor: Refactor request types

--- src/models/routes.ts
+++ src/models/routes.ts
@@ -1,11 +1,34 @@
-export interface TypedRequestBody<T> extends Express.Request {
+import { Request } from 'express';
+
+export interface RequestBody<T> extends Request {
   body: T;
-}
-export interface TypedRequestParams<T> extends Express.Request {
-  params: T;
-}
-export interface TypedRequestBodyAndParams<T, K> extends Express.Request {
-  body: T;
-  params: K;
 }
 
+export interface RequestParams<T> extends Request {
+  params: T & Request['params'];
+}
+
+export interface RequestBodyAndParams<T, K> extends Request {
+  body: T;
+  params: K & Request['params'];
+}
+
+/** Authenticated request */
+type Auth = {
+  userId: number;
+  email: string;
+}
+export interface AuthRequest extends Request {
+  body: Auth;
+}
+export interface AuthRequestBody<T> extends Request {
+  body: Auth & T;
+}
+export interface AuthRequestParams<T> extends Request {
+  body: Auth;
+  params: T & Request['params'];
+}
+export interface AuthRequestBodyAndParams<T, K> extends Request {
+  body: Auth & T;
+  params: K & Request['params'];
+}
